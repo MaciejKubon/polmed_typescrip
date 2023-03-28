c1196b45e958865c1a2dfc05dc521fa5805fb91d
MaciejKubon <maciejk2233@gmail.com>
2023-03-28 23:55:34 +0200

    2

--- src/ratingCreate.ts
+++ src/ratingCreate.ts
@@ -6,11 +6,11 @@ export const renderRating = (star:number, numberRating:number)=>{
     starsElements.classList.add('stars'); 
     for(let i=0; i<star; i++)
     {
-        starsElements.innerHTML+=`<img src="/img/star.png" alt="star">`;
+        starsElements.innerHTML+=`<img src="./img/star.png" alt="star">`;
     }
     for(let i=star; i<5; i++)
     {
-        starsElements.innerHTML+=`<img src="/img/star_white.png" alt="star">`;
+        starsElements.innerHTML+=`<img src="./img/star_white.png" alt="star">`;
     }
     doctorRatingElement.appendChild(starsElements);
     doctorRatingElement.innerHTML+=`<h3>${numberRating}</h3>`
